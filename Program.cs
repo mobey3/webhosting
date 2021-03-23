1c1a445f9a6d539fca36813a78335de955cd51e3
mobey3 <81130033+mobey3@users.noreply.github.com>
2021-03-23 16:06:38 +0900

    0323 C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _0323
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inch = Console.ReadLine();
+            double a = (double.Parse(inch));
+            double cm = 2.54;
+            Console.WriteLine("inch: " + inch);
+            Console.WriteLine(double.Parse(inch) * cm);
+
+            string kg = Console.ReadLine();
+            double b = (double.Parse(kg));
+            double pound = 2.20462262;
+            Console.WriteLine("kg: " + kg);
+            Console.WriteLine(double.Parse(kg) * pound);
+
+            string radius = Console.ReadLine();
+            double c = (double.Parse(radius));
+            double num = 3.14;
+
+            Console.WriteLine("반지름: " + radius);
+            Console.WriteLine(2*double.Parse(radius) * num );
+            Console.WriteLine(num * double.Parse(radius) * double.Parse(radius));*
+
+        }
+    }
+}
